87b142cfd5d98c19c81ae4020fb4f39fb98acc78
JuanJoseEspitia <78161123+JuanJoseEspitia@users.noreply.github.com>
2021-10-06 22:24:33 -0500

    Add files via upload

---
+++ Empleados.java
@@ -0,0 +1,24 @@
+package Clases;
+
+public class Empleados {
+	
+	int cedula;
+	String nombre;
+	String dirección;
+	int celular;
+	String correoElectrónico;
+	int salario;
+	String fechaIngreso;
+	String cumpleAños;
+	
+	void ConsularNom() {
+		System.out.println("Aquí consulta la nómina.");
+	}
+	void ConsultarLiqu() {
+		System.out.println("Aquí Consulta la liquidación.");
+	}
+	void ActualizarDatos() {
+		System.out.println("Aquí Actualiza sus datos.");
+	}
+
+}
