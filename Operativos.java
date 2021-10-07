49f9d20f36bf097705832e4fe416e80f59517e7d
JuanJoseEspitia <78161123+JuanJoseEspitia@users.noreply.github.com>
2021-10-06 22:25:54 -0500

    Add files via upload

---
+++ Operativos.java
@@ -0,0 +1,17 @@
+package Clases;
+
+public class Operativos extends Empleados {
+	
+	String cursosRealizados;
+	String ubicacionZonaTrabajo;
+	String oficioRealiza;
+	String labores;
+	
+	void ConsultarCostos() {
+		System.out.println("Aquí consulta los costos. ");
+	}
+	void ActualizarCursos() {
+		System.out.println("Aquí actualiza curso. ");
+	}
+
+}
